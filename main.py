d2cbbf16b0804e89ce1287a8724fa35b00b53abf
normalwalrus <ianchanjiajun@gmail.com>
2025-02-05 16:03:17 +0800

    feat: added AMI dataset code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from models.pyannote import PYANNOTE
 from models.nemo import DiarInference
 from models.reverb import REVERB
 
-EXAMPLE_AUDIO_PATH = 'examples/finance_zoom_meeting_processed.wav'
+EXAMPLE_AUDIO_PATH = 'examples/IS1002c.Mix-Headset.wav'
 
 def main():
 
@@ -10,11 +10,11 @@ def main():
     reverb_model = REVERB()
     nemo_model = DiarInference()
 
-    # diarization = nemo_model.diarize_to_rttm(EXAMPLE_AUDIO_PATH, 'outputs/nemo_test.rttm')
-    # print(diarization)
+    diarization = nemo_model.diarize_to_rttm(EXAMPLE_AUDIO_PATH, 'outputs/nemo_test.rttm')
+    print(diarization)
 
-    # diarization = pyannote_model.diarize_into_rttm(EXAMPLE_AUDIO_PATH, 'outputs/pyannote_test.rttm')
-    # print(diarization)
+    diarization = pyannote_model.diarize_into_rttm(EXAMPLE_AUDIO_PATH, 'outputs/pyannote_test.rttm')
+    print(diarization)
 
     diarization = reverb_model.diarize_into_rttm(EXAMPLE_AUDIO_PATH, 'outputs/reverb_test.rttm')
     print(diarization)
