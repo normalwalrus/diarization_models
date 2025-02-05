f26e97dc2bddc8a9ea254181f594c1b06ebc3246
normalwalrus <ianchanjiajun@gmail.com>
2025-02-05 23:27:04 +0800

    feat: added dataset diarization and a notebook for calculating DER with the .rttm files, also outputs of the 3 models are attached

--- main_dataset.py
+++ main_dataset.py
@@ -13,17 +13,30 @@ def main():
     pyannote_model = PYANNOTE()
     reverb_model = REVERB()
     nemo_model = DiarInference()
+
+    rttm_files_done = [f for f in os.listdir('outputs/reverb') if f.endswith(".rttm")]
+    rttm_basename_done_list = []
+    for rttm_file in rttm_files_done:
+        rttm_basename_done, _ = os.path.splitext(rttm_file)
+        rttm_basename_done_list.append(rttm_basename_done)
+
     count = 0
 
     for audio_file in LIST_OF_AUDIO_FILES:
 
+        # Prevent duplicate diarization (Assuming reverb is the last diarizer to finish)
         audio_filepath = DIARIZATION_DATASET_PATH + '/' + audio_file
         basename, _ = os.path.splitext(audio_file)
+
+        if basename in rttm_basename_done_list:
+            print(f"Skipped {audio_file}: Already diarized")
+            continue
+
         print(f"Diarization for {audio_filepath} :")
 
         diarization = nemo_model.diarize_to_rttm(audio_filepath, f'outputs/nemo/{basename}.rttm')
         diarization = pyannote_model.diarize_into_rttm(audio_filepath, f'outputs/pyannote/{basename}.rttm')
-        diarization = reverb_model.diarize_into_rttm(audio_file, f'outputs/reverb/{basename}.rttm')
+        diarization = reverb_model.diarize_into_rttm(audio_filepath, f'outputs/reverb/{basename}.rttm')
 
         count += 1
         if count % 10 == 0:
